c8ec154010bbd17293f02002bea89b9505032210
aarnav1 <65185590+aarnav1@users.noreply.github.com>
2020-12-11 20:22:43 -0500

    Add files via upload

---
+++ Screens/BookTransactionScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {text, View, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default class TransactionScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Issue/Return
+                </Text>
+            </View>    
+        )
+    }
+}
---
+++ Screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {text, View, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Search                
+                </Text>
+            </View>    
+        )
+    }
+}
